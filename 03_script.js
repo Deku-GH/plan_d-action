c3c61aec42a84d178b4a9b21c14f2ab4a4adabcf
mouad <mouadzed1@gmail.com>
2025-12-01 22:43:53 +0100

    add  Afficher les noms de plus de 5 lettres

--- 03_script.js
+++ 03_script.js
@@ -1,12 +1,12 @@
 let numbr = new Array(1, 3, 5, 7, 9)
 console.log(numbr);
 
-    console.log("for");
+console.log("for");
 for (let i = 0; i < numbr.length; i++) {
     console.log(numbr[i]);
 }
 
-    console.log("wihle");
+console.log("wihle");
 let i = 0;
 while (i < numbr.length) {
     console.log(numbr[i]);
@@ -15,3 +15,21 @@ while (i < numbr.length) {
 
 
 //la somme de tableau
+let sum = 0;
+numbr.forEach(num => {
+    sum += num;
+})
+console.log("la somme de tableau et :" + sum);
+
+
+//Afficher les noms de plus de 5 lettres
+console.log("Afficher les noms de plus de 5 lettres");
+
+let fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+fruits.forEach(fruit => {
+    if (fruit.length == 5) {
+        console.log(fruit);
+
+    }
+})
